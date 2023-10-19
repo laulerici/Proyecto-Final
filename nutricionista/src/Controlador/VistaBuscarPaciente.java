8b4579e18980cedf913da0e5ebc112cb3653fd00
facundocs2484 <141705639+facundocs2484@users.noreply.github.com>
2023-10-19 17:31:12 -0300

    19/10 CAMBIOS

--- nutricionista/src/Controlador/VistaBuscarPaciente.java
+++ nutricionista/src/Controlador/VistaBuscarPaciente.java
@@ -126,9 +126,8 @@ private Paciente pacienteActual = null ;
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel4)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jtnombre, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addComponent(jtnombre, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addContainerGap(320, Short.MAX_VALUE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addGroup(layout.createSequentialGroup()
@@ -145,6 +144,10 @@ private Paciente pacienteActual = null ;
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(jButton3)))
                         .addGap(19, 19, 19))))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(44, 44, 44)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -185,7 +188,7 @@ private Paciente pacienteActual = null ;
                     .addComponent(jButton2)
                     .addComponent(jButton3)
                     .addComponent(jButton4))
-                .addGap(0, 19, Short.MAX_VALUE))
+                .addGap(0, 23, Short.MAX_VALUE))
         );
 
         pack();
--- nutricionista/src/accesoADatos/XYSeries.java
+++ nutricionista/src/accesoADatos/XYSeries.java
@@ -5,10 +5,49 @@
  */
 package accesoADatos;
 
+import entidades.Consulta;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author LAURA
  */
 public class XYSeries {
+    private Connection con = null;
+
+    public XYSeries () {
+
+        con = Conexion.conectar("nutricionista");
+
+    }
+
+    public void agregarUsuario(String usuario, String password) {
+        String sql = "insert int usuarios (usuario, password, tipoUsuario) VALUE (?,?)"; try {
+            PreparedStatement ps = con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, usuario);
+            ps.setString(2, password);
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            consulta.setIdConsulta(rs.getInt(1));
+
+            if (rs.next()) {
+
+                JOptionPane.showMessageDialog(null, "Consulta agregada con éxito");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al agregar consulta " + ex);
+        }
+        
+                
     
+}
 }
