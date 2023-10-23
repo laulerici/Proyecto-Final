d2415710e4b2139367cae2f9c690c27487d0338f
laulerici <laulerici@hotmail.com>
2023-10-23 15:34:10 -0300

    23-10-23

--- nutricionista/src/Controlador/Home.java
+++ nutricionista/src/Controlador/Home.java
@@ -384,10 +384,10 @@ public class Home extends javax.swing.JFrame {
         this.setLocation(X-MouseX, Y-MouseY);
     }//GEN-LAST:event_formMouseDragged
 
-    private void Button_CloseMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Button_CloseMouseExited
+    private void Button_CloseMouseExited(java.awt.event.MouseEvent evt) {                                         
         Panel_Close.setBackground(Color.white);
         Button_Close.setForeground(Color.black);
-    }//GEN-LAST:event_Button_CloseMouseExited
+    }                                        
 
     private void Button_CloseMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Button_CloseMouseEntered
         Panel_Close.setBackground(Color.red);
@@ -400,17 +400,17 @@ public class Home extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_Button_CloseMouseClicked
 
-    private void Button_MinMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Button_MinMouseExited
+    private void Button_MinMouseExited(java.awt.event.MouseEvent evt) {                                       
         Panel_Min.setBackground(Color.white);
         Button_Min.setForeground(Color.black);
-    }//GEN-LAST:event_Button_MinMouseExited
+    }                                      
 
     private void Button_MinMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Button_MinMouseEntered
         Panel_Min.setBackground(Color.lightGray);
         Button_Min.setForeground(Color.white);
     }//GEN-LAST:event_Button_MinMouseEntered
 
-<<<<<<< HEAD
+
     private void Button_CloseMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Button_CloseMouseExited
         Panel_Close.setBackground(Color.white);
         Button_Close.setForeground(Color.black);
@@ -421,17 +421,17 @@ public class Home extends javax.swing.JFrame {
         Button_Min.setForeground(Color.black);
     }//GEN-LAST:event_Button_MinMouseExited
 
-    private void Button_HomeMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Button_HomeMouseEntered
+    private void Button_HomeMouseEntered(java.awt.event.MouseEvent evt) {                                         
         jPanel7.setBackground(Color.LIGHT_GRAY);
         Button_Home.setText("Inicio");
-    }//GEN-LAST:event_Button_HomeMouseEntered
-=======
+    }                                        
+
     private void Button_MinMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Button_MinMouseClicked
         if(MouseEvent.BUTTON1 == evt.getButton()){
             this.setState(Home.ICONIFIED);
         }
     }//GEN-LAST:event_Button_MinMouseClicked
->>>>>>> 77b9e3fa16cd6457915e68364cb63193dcd50a34
+
 
     private void Button_HomeMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Button_HomeMouseExited
         jPanel7.setBackground(new Color(204,204,255));
