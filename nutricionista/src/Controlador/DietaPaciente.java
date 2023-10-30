dd9e13c51ae04510b6c18e988f1518386a17c2cc
judifu23 <89588103+judifu23@users.noreply.github.com>
2023-10-30 09:10:15 -0300

    cambio

--- nutricionista/src/Controlador/DietaPaciente.java
+++ nutricionista/src/Controlador/DietaPaciente.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Controlador;
 
-/**
- *
- * @author LAURA
- */
+
 public class DietaPaciente extends javax.swing.JInternalFrame {
 
-    /**
-     * Creates new form DietaPaciente
-     */
+
     public DietaPaciente() {
         initComponents();
     }
